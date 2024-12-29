4c791f0edcc40e4cedc9fa76d01b3a8d6053b7a8
Carlos Eduardo <carlostecnicowl@gmail.com>
2024-12-28 21:33:47 -0300

    +++++++++++++

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,12 @@ module.exports = defineConfig({
   e2e: {
     experimentalStudio: true,
     baseUrl: 'https://www.saucedemo.com',
+    pageLoadTimeout: 120000,
+    //waitForAnimations: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
       
     },
+    
   },
 });
